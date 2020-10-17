1129cba4154021f0fcf39664e647890c358b0b5a
Philipp <43653566+bumagaga@users.noreply.github.com>
2020-10-17 21:04:06 +0300

    feat(helpers): добавлен список констант
    - список серверов
    - список фракций

---
+++ src/helpers/index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const serverNames = [
+  'Phoenix',
+  'Tucson',
+  'Scottdale',
+  'Chandler',
+  'Brainburg',
+  'SaintRose',
+  'Mesa',
+  'Red-Rock',
+  'Yuma',
+  'Surprise',
+  'Prescott',
+  'Glendale',
+  'Kingman',
+];
+
+const fractionNames = [
+  'Полиция г. Лос-Сантос',
+  'Областная полиция',
+  'ФБР',
+  'Полиция г.Сан-Фиерро',
+  'Больница г. Лос-Сантос',
+  'Правительство',
+  'Тюрьма строгого режима',
+  'Больница г. Сан-Фиерро',
+  'Инструкторы',
+  'Новостное агенство',
+  'Grove Street Gang',
+  'Los Santos Vagos Gang',
+  'The Ballas Gang',
+  'Varios Los Aztecas Gang',
+  'The Rifa Gang',
+  'Russian Mafia',
+  'Yakuza',
+  'La Cosa Nostra',
+  'Warlock MC',
+  'Армия г. Лос-Сантос',
+  'Центральный Банк',
+  'Больница г. Лас-Вентура',
+  'Полиция г. Лас-Вентурас',
+  'Новостное агенство ЛВ',
+  'Night Wolfs',
+  'Новостное агенство СФ',
+  'Армия г.Сан-Фиерро',
+];
+
+module.exports = {
+  serverNames,
+  fractionNames,
+};
